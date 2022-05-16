f6fec11673b87660ae7306bf6454d27583338257
harika-yejj <harikayejjuvarapu@gmail.com>
2022-05-16 17:05:19 +0530

    Project Object

---
+++ src/test/java/com/project/reactspringboot/ReactspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.reactspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
